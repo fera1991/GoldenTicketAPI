da0b627e371eb4156e067e4ca49ec57b34808b46
fera1991 <00120520@uca.edu.sv>
2023-07-01 03:58:00 -0600

    last update

--- GoldenTicketRestfulAPI/src/main/java/com/group15/goldenticket/configs/WebSecurityConfiguration.java
+++ GoldenTicketRestfulAPI/src/main/java/com/group15/goldenticket/configs/WebSecurityConfiguration.java
@@ -55,7 +55,7 @@ public class WebSecurityConfiguration {
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.httpBasic(Customizer.withDefaults()).csrf(csrf -> csrf.disable());
-		http.cors(Customizer.withDefaults());
+		http.cors(); 
 	    
 	    //Route filter
 	    http.authorizeHttpRequests(auth -> 
